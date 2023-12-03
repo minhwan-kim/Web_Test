86a8418c5fd5015390d79f7993366167b7575470
minhwan-kim <minhwan.kim.114@gmail.com>
2023-12-02 19:45:09 -0500

    touch update

--- public/script_mobile.js
+++ public/script_mobile.js
@@ -1,4 +1,4 @@
-// Event listeners for weight sorting
+// Event listeners for sorting
 document.getElementById('sortByWeightAsc').addEventListener('click', function() {
     sortProjects('weight', 'asc');
 });
@@ -7,7 +7,6 @@ document.getElementById('sortByWeightDesc').addEventListener('click', function()
     sortProjects('weight', 'desc');
 });
 
-// Event listeners for size sorting
 document.getElementById('sortBySizeAsc').addEventListener('click', function() {
     sortProjects('size', 'asc');
 });
@@ -16,22 +15,19 @@ document.getElementById('sortBySizeDesc').addEventListener('click', function() {
     sortProjects('size', 'desc');
 });
 
-// Event listeners for date sorting
 document.getElementById('sortByDateAsc').addEventListener('click', function() {
-    sortProjects('date', 'desc'); // Assuming newer dates have larger values
+    sortProjects('date', 'desc');
 });
 
 document.getElementById('sortByDateDesc').addEventListener('click', function() {
-    sortProjects('date', 'asc'); // Assuming older dates have smaller values
+    sortProjects('date', 'asc');
 });
 
 function sortProjects(attribute, direction) {
     let projects = Array.from(document.querySelectorAll('.project'));
-
     projects.sort(function(a, b) {
         let valueA = a.getAttribute(`data-${attribute}`);
         let valueB = b.getAttribute(`data-${attribute}`);
-
         if (attribute === 'date') {
             valueA = new Date(valueA);
             valueB = new Date(valueB);
@@ -39,7 +35,6 @@ function sortProjects(attribute, direction) {
             valueA = parseInt(valueA, 10);
             valueB = parseInt(valueB, 10);
         }
-
         return direction === 'asc' ? valueA - valueB : valueB - valueA;
     });
 
@@ -49,11 +44,10 @@ function sortProjects(attribute, direction) {
 
 function showFigure(figures, index, project) {
     figures.forEach(figure => {
-        figure.style.display = 'none'; // Hide all figures
+        figure.style.display = 'none';
     });
-    figures[index].style.display = 'block'; // Show the current figure
+    figures[index].style.display = 'block';
 
-    // Update the image count for the project
     let imageCountInfo = `${index + 1}/${figures.length}`;
     let imageCountDisplay = project.querySelector('.image-count');
     if (!imageCountDisplay) {
@@ -64,50 +58,35 @@ function showFigure(figures, index, project) {
     imageCountDisplay.innerText = imageCountInfo;
 }
 
-// Touch event handlers
-function handleTouchStart(evt) {
-    const img = evt.target.closest('figure').querySelector('img');
-    this.xDown = evt.touches[0].clientX;
-    this.yDown = evt.touches[0].clientY;
-    this.isTap = true; // Assume it's a tap until proven otherwise
-    this.currentFigure = img;
-}
+// Combined event handler for both touch and mouse events
+function handleInteraction(evt) {
+    // Determine if the event is a touch event
+    let isTouchEvent = evt.type.startsWith('touch');
+    let clientX = isTouchEvent ? evt.touches[0].clientX : evt.clientX;
 
-function handleTouchMove(evt) {
-    if (!this.xDown || !this.yDown) {
-        return;
+    // Handle the start of an interaction
+    if (evt.type === 'touchstart' || evt.type === 'mousedown') {
+        this.xDown = clientX;
+        this.isInteracting = true; // Flag to track if interaction is happening
+        this.currentFigure = evt.target.closest('figure').querySelector('img');
     }
 
-    var xDiff = this.xDown - evt.touches[0].clientX;
-    var yDiff = this.yDown - evt.touches[0].clientY;
-
-    if (Math.abs(xDiff) > 10 || Math.abs(yDiff) > 10) {
-        this.isTap = false; // Not a tap, it's a swipe
-    }
-}
-
-function handleTouchEnd(evt) {
-    if (this.isTap) {
+    // Handle the end of an interaction
+    if ((isTouchEvent && evt.type === 'touchend') || (!isTouchEvent && evt.type === 'mouseup' && this.isInteracting)) {
         let rect = this.currentFigure.getBoundingClientRect();
         let x = this.xDown - rect.left;
         let figures = Array.from(this.currentFigure.closest('.project').querySelectorAll('figure'));
         let currentIndex = figures.indexOf(this.currentFigure.closest('figure'));
 
         if (x < rect.width / 2) {
-            // Left half tapped
             currentIndex = currentIndex > 0 ? currentIndex - 1 : figures.length - 1;
         } else {
-            // Right half tapped
             currentIndex = currentIndex < figures.length - 1 ? currentIndex + 1 : 0;
         }
 
         showFigure(figures, currentIndex, this.currentFigure.closest('.project'));
+        this.isInteracting = false;
     }
-
-    // Reset values
-    this.xDown = null;
-    this.yDown = null;
-    this.isTap = false;
 }
 
 // Initialize projects on DOMContentLoaded
@@ -115,18 +94,16 @@ document.addEventListener('DOMContentLoaded', function() {
     var projects = document.querySelectorAll('.project');
     projects.forEach(function(project) {
         let figures = project.querySelectorAll('figure');
-        project.xDown = null;
-        project.yDown = null;
-        project.isTap = false;
-        project.currentFigure = null;
+        project.isInteracting = false;
 
         figures.forEach((figure, index) => {
-            figure.addEventListener('touchstart', handleTouchStart.bind(project), false);
-            figure.addEventListener('touchmove', handleTouchMove.bind(project), false);
-            figure.addEventListener('touchend', handleTouchEnd.bind(project), false);
+            // Combined touch and mouse event listeners
+            figure.addEventListener('touchstart', handleInteraction.bind(project), false);
+            figure.addEventListener('touchend', handleInteraction.bind(project), false);
+            figure.addEventListener('mousedown', handleInteraction.bind(project), false);
+            figure.addEventListener('mouseup', handleInteraction.bind(project), false);
         });
 
-        // Initialize with the first image
         showFigure(figures, 0, project);
     });
 });
